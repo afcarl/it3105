b7c9421e0d63b305e73a8249b7edaada5495e398
iver56 <iver56@hotmail.com>
2015-10-13 17:41:12 +0200

    Implement move(direction)

--- module4/board.py
+++ module4/board.py
@@ -85,10 +85,121 @@ class Board(object):
 
     def can_move_tile_left(self, row_index, column_index):
         numbers_to_check_for = (0, self.board_values[row_index][column_index])
-        for other_column_index in range(column_index - 1, -1, -1):
+        for other_column_index in reversed(xrange(column_index)):
             if self.board_values[row_index][other_column_index] in numbers_to_check_for:
                 return True
         return False
 
     def move(self, direction):
-        pass  # TODO
+        """
+        :param direction: 0, 1, 2 or 3
+        :return:
+        """
+        if direction == self.UP:
+            return self.move_up()
+        elif direction == self.RIGHT:
+            return self.move_right()
+        elif direction == self.DOWN:
+            return self.move_down()
+        elif direction == self.LEFT:
+            return self.move_left()
+
+    def move_up(self):
+        for row_index in range(1, self.size):
+            for column_index in xrange(self.size):
+                if self.board_values[row_index][column_index] > 0:
+                    self.move_tile_up(row_index, column_index)
+
+    def move_tile_up(self, row_index, column_index):
+        value = self.board_values[row_index][column_index]
+        potential_new_row_index = None
+        for other_row_index in reversed(xrange(row_index)):
+            other_value = self.board_values[other_row_index][column_index]
+            if other_value == value:
+                # combine
+                self.board_values[other_row_index][column_index] = value + other_value
+                self.board_values[row_index][column_index] = 0
+                return
+            elif other_value == 0:
+                potential_new_row_index = other_row_index
+            else:
+                break
+        if potential_new_row_index is not None:
+            # move
+            self.board_values[row_index][column_index] = 0
+            self.board_values[potential_new_row_index][column_index] = value
+
+    def move_right(self):
+        for row_index in xrange(self.size):
+            for column_index in reversed(xrange(self.size - 1)):
+                if self.board_values[row_index][column_index] > 0:
+                    self.move_tile_right(row_index, column_index)
+
+    def move_tile_right(self, row_index, column_index):
+        value = self.board_values[row_index][column_index]
+        potential_new_column_index = None
+        for other_column_index in range(column_index + 1, self.size):
+            other_value = self.board_values[row_index][other_column_index]
+            if other_value == value:
+                # combine
+                self.board_values[row_index][other_column_index] = value + other_value
+                self.board_values[row_index][column_index] = 0
+                return
+            elif other_value == 0:
+                potential_new_column_index = other_column_index
+            else:
+                break
+        if potential_new_column_index is not None:
+            # move
+            self.board_values[row_index][column_index] = 0
+            self.board_values[row_index][potential_new_column_index] = value
+
+    def move_down(self):
+        for row_index in reversed(xrange(self.size - 1)):
+            for column_index in xrange(self.size):
+                if self.board_values[row_index][column_index] > 0:
+                    self.move_tile_down(row_index, column_index)
+
+    def move_tile_down(self, row_index, column_index):
+        value = self.board_values[row_index][column_index]
+        potential_new_row_index = None
+        for other_row_index in range(row_index + 1, self.size):
+            other_value = self.board_values[other_row_index][column_index]
+            if other_value == value:
+                # combine
+                self.board_values[other_row_index][column_index] = value + other_value
+                self.board_values[row_index][column_index] = 0
+                return
+            elif other_value == 0:
+                potential_new_row_index = other_row_index
+            else:
+                break
+        if potential_new_row_index is not None:
+            # move
+            self.board_values[row_index][column_index] = 0
+            self.board_values[potential_new_row_index][column_index] = value
+
+    def move_left(self):
+        for row_index in xrange(self.size):
+            for column_index in range(1, self.size):
+                if self.board_values[row_index][column_index] > 0:
+                    self.move_tile_left(row_index, column_index)
+
+    def move_tile_left(self, row_index, column_index):
+        value = self.board_values[row_index][column_index]
+        potential_new_column_index = None
+        for other_column_index in reversed(xrange(column_index)):
+            other_value = self.board_values[row_index][other_column_index]
+            if other_value == value:
+                # combine
+                self.board_values[row_index][other_column_index] = value + other_value
+                self.board_values[row_index][column_index] = 0
+                return
+            elif other_value == 0:
+                potential_new_column_index = other_column_index
+            else:
+                break
+        if potential_new_column_index is not None:
+            # move
+            self.board_values[row_index][column_index] = 0
+            self.board_values[row_index][potential_new_column_index] = value
--- module4/test_board.py
+++ module4/test_board.py
@@ -45,5 +45,114 @@ class TestBoard(unittest.TestCase):
         self.assertFalse(self.board.can_move(Board.DOWN))
         self.assertTrue(self.board.can_move(Board.LEFT))
 
+    def test_can_move_4(self):
+        board_values = [
+            [0, 4, 4, 2],
+            [0, 0, 2, 8],
+            [2, 0, 8, 32],
+            [0, 0, 0, 8]
+        ]
+        self.board.set_board_values(board_values)
+        self.assertTrue(self.board.can_move(Board.UP))
+        self.assertTrue(self.board.can_move(Board.RIGHT))
+        self.assertTrue(self.board.can_move(Board.DOWN))
+        self.assertTrue(self.board.can_move(Board.LEFT))
+
+    def test_do_move_right_1(self):
+        board_values = [
+            [0, 4, 4, 2],
+            [0, 0, 0, 0],
+            [0, 2, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        self.board.set_board_values(board_values)
+        self.board.move(Board.RIGHT)
+        self.assertEqual(
+            self.board.board_values,
+            [
+                [0, 0, 8, 2],
+                [0, 0, 0, 0],
+                [0, 0, 0, 2],
+                [0, 0, 0, 0]
+            ]
+        )
+
+    def test_do_move_right_2(self):
+        board_values = [
+            [4, 4, 4, 4],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        self.board.set_board_values(board_values)
+        self.board.move(Board.RIGHT)
+        self.assertEqual(
+            self.board.board_values,
+            [
+                [0, 0, 8, 8],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ]
+        )
+
+    def test_do_move_left_1(self):
+        board_values = [
+            [0, 4, 4, 2],
+            [0, 0, 2, 8],
+            [2, 0, 8, 32],
+            [0, 0, 0, 8]
+        ]
+        self.board.set_board_values(board_values)
+        self.board.move(Board.LEFT)
+        self.assertEqual(
+            self.board.board_values,
+            [
+                [8, 2, 0, 0],
+                [2, 8, 0, 0],
+                [2, 8, 32, 0],
+                [8, 0, 0, 0]
+            ]
+        )
+
+    def test_do_move_up_1(self):
+        board_values = [
+            [0, 4, 4, 0],
+            [0, 0, 4, 0],
+            [0, 2, 0, 0],
+            [0, 0, 0, 2]
+        ]
+        self.board.set_board_values(board_values)
+        self.board.move(Board.UP)
+        self.assertEqual(
+            self.board.board_values,
+            [
+                [0, 4, 8, 2],
+                [0, 2, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ]
+        )
+
+    def test_do_move_down_1(self):
+        board_values = [
+            [2, 4, 4, 2],
+            [0, 0, 2, 8],
+            [2, 0, 8, 32],
+            [0, 0, 0, 8]
+        ]
+        self.board.set_board_values(board_values)
+        self.board.move(Board.DOWN)
+        self.assertEqual(
+            self.board.board_values,
+            [
+                [0, 0, 0, 2],
+                [0, 0, 4, 8],
+                [0, 0, 2, 32],
+                [4, 4, 8, 8]
+            ]
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
