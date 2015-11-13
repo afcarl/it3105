521b1381895c9454ca27d50bd0735cb441a79dcc
iver56 <iver56@hotmail.com>
2015-11-13 13:04:44 +0100

    Calculate and show accuracy for each set in classify.py

--- project3/classify.py
+++ project3/classify.py
@@ -3,6 +3,7 @@
 from __future__ import division, print_function, unicode_literals
 import argparse
 import brainstorm as bs
+from brainstorm.handlers import PyCudaHandler
 import img_helper
 import numpy as np
 import os
@@ -20,19 +21,7 @@ class Classify(object):
         self.parse_args()
         self.parse_images()
         self.initialize_network()
-
-        for set_name, set_dict in self.sets.iteritems():
-            if self.args.print_set_names:
-                print(set_name)
-            for i in range(len(set_dict['images'])):
-                image = set_dict['images'][i]
-                answer = self.classify(image)
-                if self.args.compare and 'correct_answers' in set_dict:
-                    correct_answer = set_dict['correct_answers'][i]
-                    if answer == correct_answer:
-                        print('correct')
-                    else:
-                        print('wrong! should be {}'.format(correct_answer))
+        self.run()
 
     def parse_args(self):
         arg_parser = argparse.ArgumentParser()
@@ -80,7 +69,7 @@ class Classify(object):
             nargs='?',
             const=True,
             required=False,
-            help='Compare each classification with the correct answer',
+            help='For every classification, show whether or not it is correct',
             default=False
         )
         arg_parser.add_argument(
@@ -123,6 +112,26 @@ class Classify(object):
             help='Run the network on the demo set',
             default=False
         )
+        arg_parser.add_argument(
+            '-st',
+            '--stats',
+            dest='stats',
+            nargs='?',
+            const=True,
+            required=False,
+            help='Show accuracy stats',
+            default=False
+        )
+        arg_parser.add_argument(
+            '-r',
+            '--print-results',
+            dest='print_results',
+            nargs='?',
+            const=True,
+            required=False,
+            help='Show every resulting integer',
+            default=False
+        )
 
         self.args = arg_parser.parse_args()
         self.network_filename = self.args.network_filename
@@ -139,23 +148,29 @@ class Classify(object):
             self.fetch_mnist_data()
 
         if self.args.training:
-            self.sets['training'] = {'images': []}
+            self.sets['training'] = {'images': [], 'correct_answers': []}
             x_training = self.mnist_ds['training']['default'][:]
             y_training = self.mnist_ds['training']['targets'][:]
             for x in x_training[0]:
                 self.sets['training']['images'].append(x)
+            for y in y_training[0]:
+                self.sets['training']['correct_answers'].append(y[0])
         if self.args.validation:
-            self.sets['validation'] = {'images': []}
+            self.sets['validation'] = {'images': [], 'correct_answers': []}
             x_validation = self.mnist_ds['validation']['default'][:]
             y_validation = self.mnist_ds['validation']['targets'][:]
             for x in x_validation[0]:
                 self.sets['validation']['images'].append(x)
+            for y in y_validation[0]:
+                self.sets['validation']['correct_answers'].append(y[0])
         if self.args.test:
-            self.sets['test'] = {'images': []}
+            self.sets['test'] = {'images': [], 'correct_answers': []}
             x_test = self.mnist_ds['test']['default'][:]
             y_test = self.mnist_ds['test']['targets'][:]
             for x in x_test[0]:
                 self.sets['test']['images'].append(x)
+            for y in y_test[0]:
+                self.sets['test']['correct_answers'].append(y[0])
         if self.args.demo:
             import pickle
             demo_set_filename = 'demo_python2'
@@ -192,8 +207,7 @@ class Classify(object):
                         image_array = img_helper.read_image(file_name)
                         self.sets['input']['images'].append(image_array)
             else:
-                pass
-                # TODO: parse text file with one image for each line
+                print('Failed to parse custom input')
 
     @staticmethod
     def print_ascii(image):
@@ -207,6 +221,7 @@ class Classify(object):
 
     def initialize_network(self):
         self.network = bs.Network.from_hdf5(self.network_filename)
+        self.network.set_handler(PyCudaHandler())
 
     def classify(self, image):
         if self.args.print_ascii:
@@ -222,9 +237,36 @@ class Classify(object):
         self.network.forward_pass()
         outputs = self.network.get('FC.outputs.default')
         max_output_index = np.argmax(outputs[0][0])
-        print(max_output_index)
+        if self.args.print_results:
+            print(max_output_index)
         return max_output_index
 
+    def run(self):
+        for set_name, set_dict in self.sets.iteritems():
+            set_dict['num_correct'] = 0
+            set_dict['num_wrong'] = 0
+            if self.args.print_set_names:
+                print(set_name)
+            for i in range(len(set_dict['images'])):
+                image = set_dict['images'][i]
+                answer = self.classify(image)
+                if 'correct_answers' in set_dict:
+                    correct_answer = set_dict['correct_answers'][i]
+                    if answer == correct_answer:
+                        set_dict['num_correct'] += 1
+                        if self.args.compare:
+                            print('correct')
+                    else:
+                        set_dict['num_wrong'] += 1
+                        if self.args.compare:
+                            print('wrong! should be {}'.format(correct_answer))
+            if self.args.stats:
+                num_total = (set_dict['num_correct'] + set_dict['num_wrong'])
+                if num_total > 0:
+                    accuracy = set_dict['num_correct'] / num_total
+                    print('Accuracy', accuracy)
+                else:
+                    print('Accuracy could not be determined')
 
 if __name__ == '__main__':
     Classify()
